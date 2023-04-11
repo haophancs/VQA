764e9467ca47405933420fc6ab85ac5bebcd0154
haophancs <haophan.cs.uit@gmail.com>
2023-04-11 11:44:32 +0700

    update

--- coatt/experiment_runner_base.py
+++ coatt/experiment_runner_base.py
@@ -170,7 +170,7 @@ class ExperimentRunnerBase(object):
 
         pd.DataFrame.from_dict({
             'ga': all_ga, 'pa': all_pa
-        }).to_csv('./outputs/.csv', index=False)
+        }).to_csv('./outputs/coatt/ga_pa.csv', index=False)
 
         trans_dict = create_trans_dict(np.union1d(all_ga, all_pa), translator=translator)
         wups = [wu_palmer_similarity(trans_dict[ga], trans_dict[pa]) for ga, pa in list(zip(all_ga, all_pa))]
