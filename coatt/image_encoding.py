e62b29bbd0a98f3477a62217eb29c64a524dc331
haophancs <haophan.cs.uit@gmail.com>
2023-04-13 23:28:13 +0700

    update

--- coatt/image_encoding.py
+++ coatt/image_encoding.py
@@ -54,7 +54,7 @@ ts_image_dir = './datasets/vivqa/test'
 tr_out_dir = './outputs/coatt/tr_enc'
 va_out_dir = './outputs/coatt/va_enc'
 ts_out_dir = './outputs/coatt/ts_enc'
-DEVICE = "cuda:1"
+DEVICE = "cuda:0"
 
 model = models.resnet18(pretrained=True)
 modules = list(model.children())[:-2]
@@ -62,8 +62,8 @@ model = nn.Sequential(*modules)
 for params in model.parameters():
     params.requires_grad = False
 
-if DEVICE == 'cuda:1':
-    model = model.to('cuda:1')
+if DEVICE == 'cuda:0':
+    model = model.to('cuda:0')
 
 tr_img_dataset = VqaImgDataset(image_dir=tr_image_dir, name='train', img_prefix="")
 tr_img_dataset_loader = DataLoader(tr_img_dataset, batch_size=64, shuffle=False, num_workers=0)
